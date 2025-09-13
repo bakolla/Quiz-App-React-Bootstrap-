5e8f4919c344f00589d7c107a0c3ec27cf8b5462
bakolla <bak.olla@o2.pl>
2025-09-13 13:48:22 +0200

    koncowy

---
+++ src/components/question.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Question = (props) => {
+
+   return (
+       <div>
+           <p>Question: {props.currentIndex}/{props.allQuestions}</p>
+                      <p>{props.currentQuestion}</p>
+
+       </div>
+   )
+};
+
+export default Question;
+
+
+
